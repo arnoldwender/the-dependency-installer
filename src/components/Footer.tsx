f87d7077d173a5b9e43a6404aa61e34099d06356
Arnold Wender <arnold.wender@gmail.com>
2026-04-04 22:57:00 +0200

    [Fix] Replace Wender Media branding with Arnold Wender

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ export function Footer() {
       className="border-t border-terminal/8 pt-6 mt-4 text-center text-[0.58rem] text-terminal/20 tracking-[2px] leading-[2.2]"
     >
       <div>NPM INSTALL LIFE IS NOT RESPONSIBLE FOR EXISTENTIAL CRISES OR BLOATED node_modules</div>
-      <div>BUILT BY WENDER MEDIA -- WE HAVE 847 DEPENDENCIES IN PRODUCTION</div>
+      <div>BUILT BY ARNOLD WENDER -- WE HAVE 847 DEPENDENCIES IN PRODUCTION</div>
       <div className="text-red-500/10 mt-1">HTTP 418 -- node_modules IS ALSO A TEAPOT</div>
     </motion.div>
   );
